bac1e0857e85af7549328f40969a93c93248fe8d
Antoine LUCAS <cooluhuru@gmail.com>
2016-08-21 17:46:54 +0200

    - Integration of angular.js-server-ng-cache

--- client/app.js
+++ client/app.js
@@ -4,7 +4,12 @@ import TodoCtrl  from './controllers/Todo';
 import Routes from './routes';
 import ProductList from './directives/ProductList';
 
-
+/*
+    this example runs with the API calls cache turne on with:
+    angular.js-server-ng-cache
+    see https://github.com/a-lucas/angular.js-server-ng-cache
+    
+*/
 window['myApp'] = angular.module('myApp', ['ngResource', 'ngRoute', 'server-cache'])
     .config(Routes)
     .controller('MainCtrl', MainCtrl)
